eb7a5ffc873c8816f2338af86f1c8170eeea8620
Rena Yamadera <renapi200i@i.softbank.jp>
2021-08-15 08:35:04 +0900

    line up等を追加

---
+++ hackathon.js
@@ -0,0 +1,12 @@
+document.getElementById("antorme").addEventListener("mouseover", black);
+function black(){
+    document.getElementById("antorme").classList.add("background")
+    document.getElementById("antormeBack").classList.add("backColor")
+}
+ 
+// document.getElementById("antorme").addEventListener("mouseout", black);
+// function black(){
+//     document.getElementById("antorme").classList.remove("background")
+//     document.getElementById("antormeBack").classList.remove("backColor")
+// }
+

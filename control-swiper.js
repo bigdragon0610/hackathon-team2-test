3096359cbf552ee31ca59a3359b0049a8e264ac0
Jin Aoyagi <contact.jin02@gmail.com>
2021-08-18 21:14:28 +0900

    swiperの導入、タイトル変更

---
+++ control-swiper.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper-container', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    loop: true,
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: true,
+      },
+  });

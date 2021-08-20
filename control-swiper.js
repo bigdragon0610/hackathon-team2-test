b57a80afbb1497b4c404e619b0864312baaffbd6
Jin Aoyagi <contact.jin02@gmail.com>
2021-08-20 13:25:17 +0900

    topのスライドを実装

--- control-swiper.js
+++ control-swiper.js
@@ -1,11 +1,26 @@
 const swiper = new Swiper('.swiper-container', {
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
     loop: true,
     autoplay: {
         delay: 4000,
         disableOnInteraction: true,
-      },
-  });
+    },
+});
+
+//要素取得
+const container = document.querySelector('#swiper01');
+const nextButton = document.querySelector('#swiper01 .swiper-button-next');
+const prevButton = document.querySelector('#swiper01 .swiper-button-prev');
+//マウスオーバーイベント
+container.addEventListener('mouseenter', () => {
+    nextButton.classList.add('hover');
+    prevButton.classList.add('hover');
+});
+//マウスアウトイベント
+container.addEventListener('mouseleave', () => {
+    nextButton.classList.remove('hover');
+    prevButton.classList.remove('hover');
+});
